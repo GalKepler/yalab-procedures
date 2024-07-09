b858a37a896f1e66155909e0abcc00e4c1a84242
GalKepler <galkepler@gmail.com>
2024-07-09 19:16:38 +0300

    WIP new dmriprep

--- src/yalab_procedures/procedures/minimal_preproc/workflows/base.py
+++ src/yalab_procedures/procedures/minimal_preproc/workflows/base.py
@@ -1,5 +1,6 @@
 import nipype.pipeline.engine as pe
 from nipype.interfaces import utility as niu
+from niworkflows.interfaces.bids import BIDSDataGrabber, BIDSFreeSurferDir, BIDSInfo
 
 def init_minimal_preproc_wf(name:str) -> pe.Workflow:
     """
@@ -8,8 +9,9 @@ def init_minimal_preproc_wf(name:str) -> pe.Workflow:
     wf = pe.Workflow(name=name)
 
     inputnode = pe.Node(
-        niu.IdentityInterface(fields=["bids_directory","output_directory","work_directory","subject_id","session_id","do_smriprep",]),
+        niu.IdentityInterface(fields=["bids_directory","output_directory","work_directory","subject_id","session_id","do_smriprep"]),
         name="inputnode"
     )
+
 
     return wf
